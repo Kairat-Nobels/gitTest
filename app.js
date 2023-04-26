f7ebb9087cd60f4fb20def8b05af4e1faef6198d
AkbaralievAlisher <kgkgg28@gmail.com>
2023-04-26 15:48:01 +0600

    add app.js

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-// const mockTest = async (url = '', data = {}) => {
-//   const res = await fetch(url, {
-//     method: 'POST',
-//     headers: { 'Content-Type': 'application/json' },
-//     body: JSON.stringify(data),
-//   });
-//   return res.json();
-// };
-// mockTest('https://643fd4a0b9e6d064befff0aa.mockapi.io/items/items', { test: 1 }).then((data) =>
+const postMockTest = async (url = '', data = {}) => {
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(data),
+  });
+  return response.json();
+};
+// postMockTest('https://643fd4a0b9e6d064befff0aa.mockapi.io/items/items', { test: 1 }).then((data) =>
 //   console.log(data),
 // );
 
@@ -14,6 +14,26 @@ const getMockTest = async (url = '') => {
   const response = await fetch(url);
   return response.json();
 };
-getMockTest('https://643fd4a0b9e6d064befff0aa.mockapi.io/items/items').then((data) =>
-  console.log(data),
-);
+// getMockTest('https://643fd4a0b9e6d064befff0aa.mockapi.io/items/items').then((data) =>
+//   console.log(data),
+// );
+
+const deleteMockTest = async (url = '', id) => {
+  const response = await fetch(url, {
+    method: 'DELETE',
+  });
+  return response.json();
+};
+// deleteMockTest(`https://643fd4a0b9e6d064befff0aa.mockapi.io/items/items/${1}`).then((data) => data);
+
+const patchMockTest = async (url = '', data = {}) => {
+  const response = await fetch(url, {
+    method: 'PATCH',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(data),
+  });
+  return response.json();
+};
+patchMockTest(`https://643fd4a0b9e6d064befff0aa.mockapi.io/items/items/${2}`, {
+  testPacth: 1,
+}).then((data) => console.log(data));
